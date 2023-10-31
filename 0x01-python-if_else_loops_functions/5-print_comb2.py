3800420af8ff935897774c901cd867b1cab605dd
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-10-31 02:41:35 -0700

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 for a in range(0, 100):
     if a < 10:
-        print("0{:d}".format(a), end=", ")
-    elif a == 99:
-        print("{:d}".format(a))
+        x = "0" + str(a)
     else:
-        print("{:d}".format(a), end=", ")
+        x = str(a)
+    if a == 99:
+        print("{:d}".format(a))
+        break
+    print("{:s}".format(x), end=", ")
