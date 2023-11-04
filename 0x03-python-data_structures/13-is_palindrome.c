f79a080d10cd06dc3e9bc546597cddf790110137
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-04 12:01:46 -0700

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,69 +1,12 @@
 #include "lists.h"
-void list_rev(listint_t **head)
-{
-	listint_t *link = NULL, *curr = *head, *back = NULL;
-
-	while (curr != NULL)
-	{
-		link = curr->next;
-		curr->next = back;
-		back = curr;
-		curr = link;
-	}
-	*head = back;
-}
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr1 = *head, *ptr2 = NULL, *t = *head, *y = *head;
-
-	if (!*head || !(*head)->next)
-		return (1);
-	while (1)
-	{
-		t = t->next->next;
-		if (t == NULL)
-		{
-			ptr2 = y->next;
-			break;
-		}
-		if (t->next == NULL)
-		{
-			ptr2 = y->next->next;
-			break;
-		}
-		y = y->next;
-	}
-	list_rev(&ptr2);
-	while (ptr1 && ptr2)
-	{
-		if (ptr1->n == ptr2->n)
-		{
-			ptr1 = ptr1->next;
-			ptr2 = ptr2->next;
-		}
-		else
-			return (0);
-	}
-	if (ptr2 == NULL)
-		return (1);
-	return (0);
-}
-int is_palindrome2(listint_t **head)
-{
 	listint_t *ptr;
-	int count = 0, *arr, idx = 0, i, j;
+	int idx = 0, i;
+	int arr[50];
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	ptr = *head;
-	while (ptr != NULL)
-	{
-		ptr = ptr->next;
-		count++;
-	}
-	arr = malloc(sizeof(int) * count);
-	if (arr == NULL)
-		return (0);
 	ptr = *head;
 	while (ptr != NULL)
 	{
@@ -71,14 +14,10 @@ int is_palindrome2(listint_t **head)
 		idx++;
 		ptr = ptr->next;
 	}
-	for (i = 0, j = count - 1; i < count / 2; i++, j--)
+	for (i = 0; i < idx / 2; i++, idx--)
 	{
-		if (arr[i] != arr[j])
-		{
-			free(arr);
+		if (arr[i] != arr[idx - 1])
 			return (0);
-		}
 	}
-	free(arr);
 	return (1);
 }
