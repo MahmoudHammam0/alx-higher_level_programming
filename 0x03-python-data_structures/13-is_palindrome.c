8b5bfca06c3ce9e8663f8d2c43dd6569d0276a02
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-04 11:11:00 -0700

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,46 +1,5 @@
 #include "lists.h"
 #include <stdlib.h>
-/**void list_rev(listint_t **head)
-{
-	listint_t *link = NULL, *curr = *head, *back = NULL;
-
-	while (curr != NULL)
-	{
-		link = curr->next;
-		curr->next = back;
-		back = curr;
-		curr = link;
-	}
-	*head = back;
-}
-int is_palindrome(listint_t **head)
-{
-	listint_t *ptr1 = *head, *ptr2 = *head;
-
-	if (!*head || !(*head)->next)
-		return (1);
-	while (ptr2 != NULL)
-	{
-		if (ptr2->n == ptr2->next->n)
-			break;
-		ptr2 = ptr2->next;
-	}
-	ptr2 = ptr2->next;
-	list_rev(&ptr2);
-	while (ptr1 && ptr2)
-	{
-		if (ptr1->n == ptr2->n)
-		{
-			ptr1 = ptr1->next;
-			ptr2 = ptr2->next;
-		}
-		else
-			return (0);
-	}
-	if (ptr2 == NULL)
-		return (1);
-	return (0);
-}*/
 int is_palindrome(listint_t **head)
 {
 	listint_t *ptr;
@@ -72,11 +31,6 @@ int is_palindrome(listint_t **head)
 			return (0);
 		}
 	}
-	if (arr[i] == arr[j])
-	{
-		free(arr);
-		return (1);
-	}
 	free(arr);
-	return (0);
+	return (1);
 }
