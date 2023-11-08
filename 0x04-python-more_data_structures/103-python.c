5daf68f98bf39febeff914d5a0dd100718b918b5
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-08 15:26:18 -0800

    16. CPython #1: PyBytesObject

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -10,14 +10,14 @@ void print_python_bytes(PyObject *p)
 	long int length, idx = 0, max;
 	char *str;
 
+	length = ((PyVarObject *)(p))->ob_size;
+	str = ((PyBytesObject *)p)->ob_sval;
 	printf("[.] bytes object info\n");
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
-	length = ((PyVarObject *)(p))->ob_size;
-	str = ((PyBytesObject *)p)->ob_sval;
 	printf("  size: %ld\n", length);
 	printf("  trying string: %s\n", str);
 	if (length >= 10)
@@ -46,8 +46,8 @@ void print_python_list(PyObject *p)
 	long int length, idx = 0;
 	PyObject *ptr;
 
-	length = ((PyVarObject *)(p))->ob_size;
 	temp = (PyListObject *)p;
+	length = ((PyVarObject *)(p))->ob_size;
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", length);
 	printf("[*] Allocated = %ld\n", temp->allocated);
