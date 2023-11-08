411ac7d5c56b7a6193a2871fd5564170d7e6d7ae
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-08 08:54:35 -0800

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     my_dictionary = {'I': 1, 'V': 5, 'X': 10,
                      'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if roman_string is None or type(roman_string) is not str:
+    if (roman_string is None) or (type(roman_string) is not str):
         return (0)
     else:
         num = 0
@@ -15,5 +15,5 @@ def roman_to_int(roman_string):
                             if (roman_string[x + 1] == 'X'
                                     or roman_string[x + 1] == 'V'):
                                 num -= 2
-                    break
+                break
         return (num)
