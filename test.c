87c1e1707e63bf48878f8337d561cc429ad54131
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-12 09:32:18 -0800

    trying

---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char *argv[])
+{
+	char *add[] = {"/usr/bin/git", "add", ".", NULL};
+	char *comm[] = {"/usr/bin/git", "commit", "-m", argv[1], NULL};
+	char *push[] = {"/usr/bin/git", "push", NULL};
+	execve(add[0], add, NULL);
+	execve(comm[0], comm, NULL);
+	execve(push[0], push, NULL);
+	return (0);
+}
