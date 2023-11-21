a122f2ef72c4a5b8d9464879383ff18313e55375
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-21 04:49:43 -0800

    6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,14 +43,7 @@ class Square:
     @position.setter
     def position(self, value):
         """setter method for position"""
-        if (type(value) is not tuple or len(value) != 2):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if (type(value[0]) is not int or type(value[1]) is not int):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        if (value[0] < 0 or value[1] < 0):
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        self.__position = value
 
     def area(self):
         """calculate area of square"""
