201a2b012beab927352915de209cbc2921c83b8d
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-21 02:49:34 -0800

    1. Square with size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,6 +4,6 @@
 
 class Square:
     """class square with private attribute size"""
-    def __init__(self, size):
+    def __init__(self, size=0):
         """intialization"""
         self.__size = size
