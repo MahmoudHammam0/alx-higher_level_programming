d354acf26181bae8fce1d3fa98f78fd3a27f6432
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-11-21 04:46:01 -0800

    6. Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,12 +6,11 @@ class Square:
     """class square with private attribute size"""
     def __init__(self, size=0, position=(0, 0)):
         """intialization"""
-        if (type(self.__position) is not tuple or len(self.__position) != 2):
+        if (type(position) is not tuple or len(position) != 2):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if (type(self.__position[0]) is not int 
-                or type(self.__position[1]) is int):
+        if (type(position[0]) is not int or type(position[1]) is int):
             raise TypeError("position must be a tuple of 2 positive integers")
-        if (self.__position[0] < 0 or self.__position[1] < 0):
+        if (position[0] < 0 or position[1] < 0):
             raise TypeError("position must be a tuple of 2 positive integers")
         if (type(size) is not int):
             raise TypeError("size must be an integer")
