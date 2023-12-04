f719b7cf9c0a154a0afea67e2197fc57ed33e128
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-04 10:37:36 -0800

    4. Only sub class of

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Only sub class of module'''
+
+
+def inherits_from(obj, a_class):
+    '''check if the object is an instance of a class inherited from a_class
+
+    Args:
+        obj: object to be checked
+        a_class: class to check
+
+    Return:
+        True: if it was a sub class
+        False: otherwise
+    '''
+    if issubclass(type(obj), a_class):
+        return True
+    return False
