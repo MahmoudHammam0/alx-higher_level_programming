2e3ffe91a0e14d6f3668237ea7230f2809518a2f
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-05 09:53:18 -0800

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Read file module'''
+
+
+def read_file(filename=""):
+    '''reads a text file and print to stdout'''
+    with open(filename, encoding="UTF8") as my_file:
+        print(my_file.read())
