e9dca59870884f09e00a0d3213401276f3aa1d83
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-05 13:37:57 -0800

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,8 +10,9 @@ for i in range(len(sys.argv)):
     if i == 0:
         continue
     args_list.append(sys.argv[i])
-if args_list == []:
-    save_to_json_file(args_list, "add_item.json")
-obj = load_from_json_file("add_item.json")
+try:
+    obj = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    obj = []
 obj += args_list
 save_to_json_file(obj, "add_item.json")
