4fcfbc6017d7983bd8c97da2359de828d142884e
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-09 19:19:51 -0800

    16. JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''Base class module'''
+import json
 
 
 class Base:
@@ -13,3 +14,24 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''return json representation of list_dictionaries'''
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        '''writes the JSON string representation of list_objs to a file'''
+        filename = "{}.json".format(list_objs[0].__class__.__name__)
+        if list_objs is None:
+            content = []
+        else:
+            x = []
+            for obj in list_objs:
+                x.append(obj.to_dictionary())
+            content = Base.to_json_string(x)
+        with open(filename, mode='w', encoding='UTF8') as my_file:
+            my_file.write(content)
