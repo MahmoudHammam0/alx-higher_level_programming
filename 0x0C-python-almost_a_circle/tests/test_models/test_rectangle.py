caf3cfbefebae0740a673d470f3038ecdea35d24
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-10 18:25:13 -0800

    Unit_Test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -2,6 +2,7 @@
 '''Rectangle unittest module'''
 import unittest
 from models.rectangle import Rectangle
+from models.base import Base
 
 
 class TestRectangle(unittest.TestCase):
@@ -102,4 +103,30 @@ class TestRectangle(unittest.TestCase):
         r1 = Rectangle(3, 4)
         self.assertEqual(r1.area(), 12)
 
-    def test_display(self):
+    def test_str_method(self):
+        '''tests Rectangle str method'''
+        r1 = Rectangle(5, 6, 8, 9)
+        s = "[Rectangle] ({}) 8/9 - 5/6".format(r1.id)
+        x = str(r1)
+        self.assertEqual(x, s)
+
+    def test_update_method_args(self):
+        '''test Rectangle update method with args'''
+        r1 = Rectangle(2, 3)
+        t = [20, 5, 2, 3, 4]
+        r1.update(*t)
+        s = "[Rectangle] (20) 3/4 - 5/2"
+        x = str(r1)
+        self.assertEqual(x, s)
+
+    def test_update_method_kwargs(self):
+        '''test Rectangle update method with kwargs'''
+        r1 = Rectangle(2, 3)
+        the_dict = {'width': 1, 'x': 2, 'y': 3, 'id': 89, 'height': 5}
+        r1.update(**the_dict)
+        w = "[Rectangle] (89) 2/3 - 1/5"
+        m = str(r1)
+        self.assertEqual(m, w)
+    def test_subclass(self):
+        '''check if the Rectangle is subclass of Base'''
+        self.assertTrue(issubclass(Rectangle, Base))
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''Square class unittest module'''
+from models.square import Square
+from models.rectangle import Rectangle
+import unittest
+
+
+class TestSquare(unittest.TestCase):
+    '''unittest class for square class'''
+    def test_Rectangle_subclass(self):
+        '''check is square inherits from Rectangle'''
+        self.assertTrue(issubclass(Square, Rectangle))
+
+    def test_no_args(self):
+        '''test Square without arguments'''
+        self.assertRaises(TypeError, Square)
+
+    def test_one_arg(self):
+        '''test square with one argument'''
+        s1 = Square(5)
+        self.assertEqual(s1.size, 5)
+
+    def test_attr_x(self):
+        '''test square attribue x'''
+        s1 = Square(2, 5)
+        self.assertEqual(s1.x, 5)
+
+    def test_attr_y(self):
+        '''test square attribute y'''
+        s1 = Square(1, 3, 5)
+        self.assertEqual(s1.y, 5)
+
+    def test_Rectangle_attr(self):
+        '''test Rectangle attributes from square'''
+        s1 = Square(1, 2, 3, 4)
+        self.assertEqual(s1.width, 1)
+        self.assertEqual(s1.height, 1)
+        self.assertEqual(s1.id, 4)
+
+    def test_area_method(self):
+        '''test area from Square class'''
+        s1 = Square(5)
+        self.assertEqual(s1.area(), 25)
+
+    def test_str_method(self):
+        '''test str method of Square'''
+        s1 = Square(3, 5, 9, 120)
+        s = "[Square] (120) 5/9 - 3"
+        x = str(s1)
+        self.assertEqual(x, s)
