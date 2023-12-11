d66d5e80d327a98a2d8b1ef9433c796018c1b04f
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2023-12-11 12:57:38 -0800

    16. JSON string to file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,7 +15,7 @@ class Square(Rectangle):
     @property
     def size(self):
         '''getter for size'''
-        return self.__width
+        return self.width
 
     @size.setter
     def size(self, value):
@@ -24,9 +24,9 @@ class Square(Rectangle):
             raise TypeError('width must be an integer')
         if value <= 0:
             raise ValueError('width must be > 0')
-        self.__width = value
-        self.__height = value
-        self.__size = value
+        self.width = value
+        self.height = value
+        self.size = value
 
     def update(self, *args, **kwargs):
         '''update values of class attributes'''
