ddd3ee4746a511cfc72aea7b036ebdc8ffebf5e1
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-12 14:06:05 -0800

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const arg = process.argv;
 const array = [];
-if (!arg[2] || arg.length === 3) {
+if (arg.length <= 3) {
   console.log(0);
 } else {
   for (let i = 2; i < arg.length; i++) {
