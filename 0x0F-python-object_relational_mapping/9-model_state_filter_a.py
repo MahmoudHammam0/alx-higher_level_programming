cd90fb615593c5e644c66739e8b72c25c8114c33
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-02-17 03:42:57 -0800

    9. Contains

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,8 +12,6 @@ if __name__ == '__main__':
                            format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Session = sessionmaker(bind=engine)
     session = Session()
-    if (session.query(State).first() is None):
-        print("Nothing")
-    else:
-        state = session.query(State).first()
+    for state in session.query(State).filter(State.name.ilike('%a%')):
         print("{}: {}".format(state.id, state.name))
+    session.close()
