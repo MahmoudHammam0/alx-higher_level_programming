856567ff0681cb22c9ca2b678e7f1d9645ba2a1e
MahmoudHammam0 <MahmoudHammam235@gmail.com>
2024-02-29 03:21:40 -0800

    4. cURL headers

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#takes in a URL, sends a request to that URL, and displays the size of the body of the response
+#displays all HTTP methods the server will accept.
 curl -sI "$1" | grep Allow | cut -d ":" -f2 | cut -d" " -f2,3,4,5,6,7,8,9,10,11,12
