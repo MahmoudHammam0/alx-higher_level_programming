a4e097c29275021f806e283c508e7e768ff81de8
MahmoudHammam0 <MahmoudHammam235@gmail.com>
2024-03-01 03:55:02 -0800

    10. Time for an interview!

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''Time for an interview! module'''
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+    headers = {"Accept": "application/vnd.github+json",
+               "X-GitHub-Api-Version": "2022-11-28"
+               }
+    req = requests.get(url, headers=headers)
+    content = req.json()
+    for i in range(10):
+        print("{}: {}".format(content[i].get("sha"),
+                       content[i].get("commit")
+                       .get("author")
+                       .get("name")))
