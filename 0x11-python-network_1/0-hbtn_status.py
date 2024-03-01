372301a1162e397021422f14ce55dc0a2a533278
MahmoudHammam0 <MahmoudHammam235@gmail.com>
2024-03-01 01:06:39 -0800

    0. What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as resp:
         content = resp.read()
     print("Body response:")
-    print("    - type: {}".format(type(content)))
-    print("    - content: {}".format(content))
-    print("    - utf8 content: {}".format(content.decode()))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+    print("\t- utf8 content: {}".format(content.decode()))
