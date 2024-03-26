b535d3cc4d7eb9dee31ad8f7eaea02f802ce984b
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-26 13:43:52 -0700

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,4 +4,5 @@ const request = require('request');
 const process = require('process');
 request(process.argv[2], function (error, response, body) {
   console.log('code:', response.statusCode);
+  console.error('error:', error);
 });
