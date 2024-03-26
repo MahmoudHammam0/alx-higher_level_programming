8dbe27c088afa828d4a5cd1901cf537ff867d85d
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-26 15:59:42 -0700

    6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,7 +4,7 @@ const request = require('request');
 const process = require('process');
 const url = process.argv[2];
 
-request(url, (error, response, body) => {
+request.get(url, (error, response, body) => {
   const list = JSON.parse(body);
   const res = {};
   if (error) {
