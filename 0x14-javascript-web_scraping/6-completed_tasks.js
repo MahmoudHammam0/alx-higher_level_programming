31dae0288e3d00ac006fafaa3bddb777144d3c74
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-26 16:13:20 -0700

    6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -9,18 +9,16 @@ request.get(url, (error, response, body) => {
   const res = {};
   if (error) {
     console.log(error);
-    return;
-  }
-  for (const task of list) {
-    if (!(task.userId in res)) {
-      if (task.completed) {
+  } else {
+    list.forEach((task) => {
+    if (task.completed) {
+      if (!res[task.userId]) {
         res[task.userId] = 1;
       } else {
-        res[task.userId] = 0;
+        res[task.userId]++;
       }
-    } else if (task.userId in res && task.completed) {
-      res[task.userId]++;
     }
-  }
+  });
   console.log(res);
+  }
 });
