b40418cb23e487435245e086ebec70a93f61e1a1
MahmoudHammam0 <mahmoudhammam235@gmail.com>
2024-03-26 14:44:01 -0700

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,11 +10,11 @@ request(url, function (error, response, body) {
   } else {
     const results = JSON.parse(body).results;
     const charId = 'https://swapi-api.alx-tools.com/api/people/18/';
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].characters.includes(charId)) {
+    results.forEach(results => {
+      if (results.characters.includes(charId)) {
         count++;
       }
-    }
+    });
     console.log(count);
   }
 });
